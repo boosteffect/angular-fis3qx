570536da9b1afbe117240209167aac50c1e14db5
boosteffect <35601682+boosteffect@users.noreply.github.com>
2023-03-03 16:58:19 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { SelectableSettings } from '@progress/kendo-angular-treelist';
 import { filesystem, Entry } from './filesystem';
-import { storeList, StoreEntry } from './stores';
+import { storeList, storeList2, StoreEntry } from './stores';
 
 @Component({
   selector: 'my-app',
@@ -12,23 +12,27 @@ import { storeList, StoreEntry } from './stores';
                   kendoTreeListSelectable [selectable]="settings" [(selectedItems)]="selected"
                   kendoTreeListExpandable
                   [height]="435">
-            <kendo-treelist-checkbox-column [width]="40" [checkChildren]="true" [showSelectAll]="true">
+            
+             <kendo-treelist-checkbox-column [width]="40" [checkChildren]="true" [showSelectAll]="true">
             </kendo-treelist-checkbox-column>
+            
             <kendo-treelist-column [expandable]="true" field="name" title="SKU" [width]="140">
             </kendo-treelist-column>
+            
             <kendo-treelist-column field="desc" title="Description" >
             </kendo-treelist-column>
+            
             <kendo-treelist-column field="actualOrder" title="Actual Order" [width]="110" class="text-right" >
             </kendo-treelist-column>
-            <kendo-treelist-column field="transfered" title="Transfer" [width]="80" class="text-right" >
+            <kendo-treelist-column field="transfered" title="Transfer" [width]="80" class="text-right">
             </kendo-treelist-column>
             <kendo-treelist-column field="qtyRemaining" title="Qty To" [width]="80" class="text-right" >
             </kendo-treelist-column>
         </kendo-treelist>
 
-        <div style="padding-top:'100px;'"></div>
+        <div style="width:100%; padding-top:'100px;'">
 
-        <div style="width:'300px';">
+        <div style="width:'300px'; float: left; ">
         <kendo-treelist [kendoTreeListHierarchyBinding]="storeData" childrenField="contents" idField="id"
                   kendoTreeListSelectable [selectable]="settings" [(selectedItems)]="selected"
                   kendoTreeListExpandable
@@ -36,18 +40,38 @@ import { storeList, StoreEntry } from './stores';
                   >
             <kendo-treelist-checkbox-column [width]="40" [checkChildren]="true" [showSelectAll]="true">
             </kendo-treelist-checkbox-column>
-            <kendo-treelist-column [expandable]="true" field="warehouse" title="Dist Warehouse"  [width]="170">
+            
+            <kendo-treelist-column [expandable]="true" field="warehouse" title="Dist Warehouse" [width]="170"
+            [style]="{'font-weight': 'bold'}">
             </kendo-treelist-column>
+            
             <kendo-treelist-column field="store" title="Store" class="text-right" >
             </kendo-treelist-column>
  
         </kendo-treelist>
         </div>
-     `,
+
+        <div style="width:'230px'; float: right; ">
+        <kendo-treelist [kendoTreeListHierarchyBinding]="storeData2" childrenField="contents" idField="id"
+                  kendoTreeListSelectable [selectable]="settings" [(selectedItems)]="selected"
+                  kendoTreeListExpandable
+                  [height]="420"
+                  >
+            <kendo-treelist-checkbox-column [width]="40" [checkChildren]="true" [showSelectAll]="true">
+            </kendo-treelist-checkbox-column>
+
+            <kendo-treelist-column [expandable]="true" field="store" title="Store" >
+            </kendo-treelist-column>
+ 
+        </kendo-treelist>
+        </div>
+        </div>
+      `,
 })
 export class AppComponent {
   public data: Entry[] = filesystem;
   public storeData: StoreEntry[] = storeList;
+  public storeData2: StoreEntry[] = storeList2;
 
   public settings: SelectableSettings = {
     mode: 'row',
--- src/app/stores.ts
+++ src/app/stores.ts
@@ -221,3 +221,214 @@ export const storeList: StoreEntry[] = [
   },
 
 ];
+
+export const storeList2: StoreEntry[] = [
+  {
+    type: 'group',
+    id: nextId(),
+    warehouse: '4501 - Lakeland ',
+    store: '4501 - Lakeland DC',
+    contents: [
+      {
+        type: 'item',
+        id: nextId(),
+        store: '5401',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1547',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '732',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1517',
+      },    
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1347',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1651',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '681',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1004',
+      }, 
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1371',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '129',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1670',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1681',
+      },     
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1212',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '356',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '836',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '712',
+      },      
+      {
+        type: 'item',
+        id: nextId(),
+        store: '878',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '793',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1135',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1260',
+      },   
+    ],
+  },
+  {
+    type: 'group',
+    id: nextId(),
+    warehouse: '4503 - Jacksonville',
+    store: '4503 - Jacksonville',
+    contents: [
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1547',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '732',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1517',
+      },    
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1347',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1651',
+      },
+    ],
+  },
+  {
+    type: 'group',
+    id: nextId(),
+    warehouse: '4506 - Deerfield',
+    store: '4506 - Deerfield',
+    contents: [
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1547',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '732',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1517',
+      },    
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1347',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1651',
+      },
+    ],
+  },
+  {
+    type: 'group',
+    id: nextId(),
+    warehouse: '4508 - Orlando',
+    store: '4508 - Orlando',
+    contents: [
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1547',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '732',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1517',
+      },    
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1347',
+      },
+      {
+        type: 'item',
+        id: nextId(),
+        store: '1651',
+      },
+    ],
+  },
+
+];
