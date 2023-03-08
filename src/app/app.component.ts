1a216069cd1d502a61b7c99144f4fdf978f16a42
boosteffect <35601682+boosteffect@users.noreply.github.com>
2023-03-08 14:44:55 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {
+  filterBy,
+  CompositeFilterDescriptor,
+} from "@progress/kendo-data-query";
 import { SelectableSettings } from '@progress/kendo-angular-treelist';
 import { filesystem, Entry } from './filesystem';
 import { storeList, storeList2, StoreEntry } from './stores';
@@ -10,7 +14,8 @@ import { storeList, storeList2, StoreEntry } from './stores';
         <kendo-treelist [kendoTreeListHierarchyBinding]="data" childrenField="contents" idField="id"
                   [initiallyExpanded]="true"
                   kendoTreeListSelectable [selectable]="settings" [(selectedItems)]="selected"
-                  kendoTreeListExpandable
+                  kendoTreeListExpandable [filterable]="true"
+                  [filter]="itemfilter"
                   [height]="480">
             
              <kendo-treelist-checkbox-column [width]="40" [checkChildren]="true" [showSelectAll]="true">
@@ -22,40 +27,43 @@ import { storeList, storeList2, StoreEntry } from './stores';
             <kendo-treelist-column field="desc" title="Description" >
             </kendo-treelist-column>
             
-            <kendo-treelist-column field="actualOrder" title="Actual Order" [width]="110" class="text-right" >
+            <kendo-treelist-column field="actualOrder" title="Actual Order" [width]="110" [filterable]="false" class="text-right" >
             </kendo-treelist-column>
-            <kendo-treelist-column field="transfered" title="Transfer" [width]="80" class="text-right">
+            <kendo-treelist-column field="transfered" title="Transfer" [width]="80" class="text-right" [filterable]="false">
             </kendo-treelist-column>
-            <kendo-treelist-column field="qtyRemaining" title="Qty To" [width]="80" class="text-right" >
+            <kendo-treelist-column field="qtyRemaining" title="Qty To" [width]="80" class="text-right" [filterable]="false">
             </kendo-treelist-column>
         </kendo-treelist>
 
-        <div style="height:1000px; width:100%; padding-top:'10px;'">
+        <div style="height:1000px; width:100%; padding-top:'50px;'">
 
-        <div style="width:'300px'; float: left; ">
+        <div style="width:'360px'; float: left; ">
         <kendo-treelist [kendoTreeListHierarchyBinding]="storeData" childrenField="contents" idField="id"
                   kendoTreeListSelectable [selectable]="settings" [(selectedItems)]="selected"
                   kendoTreeListExpandable
-                  [height]="480"
+                  [height]="480" [filterable]="true"
+                  [filter]="storefilter"
                   >
             <kendo-treelist-checkbox-column [width]="40" [checkChildren]="true" [showSelectAll]="true">
             </kendo-treelist-checkbox-column>
             
-            <kendo-treelist-column [expandable]="true" field="warehouse" title="Dist Warehouse" [width]="170"
+            <kendo-treelist-column [expandable]="true" field="warehouse" title="Dist Warehouse" [width]="170" [filterable]="false"
             [style]="{'font-weight': 'bold'}">
             </kendo-treelist-column>
             
-            <kendo-treelist-column field="store" title="Store" class="text-right" >
+            <kendo-treelist-column field="store" title="Store" class="text-right" > 
             </kendo-treelist-column>
  
         </kendo-treelist>
         </div>
 
         <div style="width:'230px'; float: right; ">
-        <kendo-treelist [kendoTreeListHierarchyBinding]="storeData2" childrenField="contents" idField="id"
+
+        <kendo-treelist style="display:none" [kendoTreeListHierarchyBinding]="storeData2" childrenField="contents" idField="id"
                   kendoTreeListSelectable [selectable]="settings" [(selectedItems)]="selected"
                   kendoTreeListExpandable
-                  [height]="545"
+                  [height]="545" [filterable]="true"
+                  [filter]="storefilter"
                   >
             <kendo-treelist-checkbox-column [width]="40" [checkChildren]="true" [showSelectAll]="true">
             </kendo-treelist-checkbox-column>
@@ -73,6 +81,16 @@ export class AppComponent {
   public storeData: StoreEntry[] = storeList;
   public storeData2: StoreEntry[] = storeList2;
 
+  public itemfilter: CompositeFilterDescriptor = {
+    logic: "and",
+    filters: [],
+  };
+  
+  public storefilter: CompositeFilterDescriptor = {
+    logic: "and",
+    filters: [],
+  };
+
   public settings: SelectableSettings = {
     mode: 'row',
     multiple: true,
@@ -80,4 +98,6 @@ export class AppComponent {
   };
 
   public selected: any[] = [];
+
+
 }
